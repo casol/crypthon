b5d640c6a40d637895f628d7a5fc34f58aaee456
casol <casoltys@gmail.com>
2018-07-04 23:45:49 +0200

    Update models

--- account/models.py
+++ account/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.conf import settings
 
+import uuid
+
 
 class Profile(models.Model):
     """Extends default Django User model to store information related to the User.
 
     The Profile model stores the data required to authenticate the user account.
-    """
 
+    """
+    # on_delete=models.PROTECT will raise an IntegrityError
+    # when attempting to delete a user with an account
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE)
+                                on_delete=models.PROTECT)
+    uid = models.UUIDField(unique=True, editable=False,
+                           default=uuid.uuid4, verbose_name='Public identifier', )  # A public id
     verified = models.NullBooleanField(blank=True)
     date_of_birth = models.DateField(blank=True, null=True)
     document_country_of_issue = models.CharField(max_length=100, blank=True,
--- wallet/models.py
+++ wallet/models.py
@@ -11,14 +11,14 @@ class Wallet(models.Model):
      """
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    id = models.AutoField(primary_key=True,)  # A private identifier
-    uid = models.UUIDField(unique=True, editable=False,
-                           default=uuid.uuid4, verbose_name='Public identifier',)  # A public id
     currency = models.ForeignKey('Currency', on_delete=models.PROTECT)
     # max digits? There will be ~21mln of btc, 84mln of ltc but what with xrp or something else?
     balance = models.DecimalField(max_digits=18, decimal_places=8,
                                   default=0)
+    created = models.DateTimeField(blank=True)
     modified = models.DateTimeField(blank=True)
+    active = models.BooleanField(default=False)
+    transaction_counter = models.IntegerField(default=1)
 
     class Meta:
         verbose_name = 'Wallet'
@@ -27,4 +27,15 @@ class Wallet(models.Model):
 
 
 class Currency(models.Model):
+    pass
+
+class Address(models.Model):
+    """
+    In simple terms a wallet address is similar to a bank account number.
+    It’s a unique 26-35 digit combination of letters and numbers and it
+    looks something like this: 1ExAmpLe0FaBiTco1NADr3sSV5tsGaMF6hd
+    """
+    pass
+
+class Action(models.Model):
     pass
