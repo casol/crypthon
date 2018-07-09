dcde65d108a521f47e5c10db8b28fc46654a0622
casol <casoltys@gmail.com>
2018-07-09 23:20:50 +0200

    Update Address model
    I will start with a simplified model and with time improve app models

--- wallet/models.py
+++ wallet/models.py
@@ -15,7 +15,7 @@ class Wallet(models.Model):
     # max digits? There will be ~21mln of btc, 84mln of ltc but what with xrp or something else?
     balance = models.DecimalField(max_digits=18, decimal_places=8,
                                   default=0)
-    created = models.DateTimeField(blank=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
     modified = models.DateTimeField(blank=True)
     active = models.BooleanField(default=False)
     transaction_counter = models.IntegerField(default=1)
@@ -35,7 +35,16 @@ class Address(models.Model):
     It’s a unique 26-35 digit combination of letters and numbers and it
     looks something like this: 1ExAmpLe0FaBiTco1NADr3sSV5tsGaMF6hd
     """
-    pass
+    address = models.CharField(max_length=50)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    label = models.CharField(max_length=50, blank=True, null=True, default=None)
+    wallet = models.ForeignKey('Wallet', null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '{}, {}'.format(self.address, self.currency)
+    
 
 class Action(models.Model):
     pass
